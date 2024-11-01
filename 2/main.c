c49803874ec4aa363068f4727eec57f27fbf84e0
meow woof <meow@woof.com>
2024-11-01 17:20:50 +0300

    idk man it's a commit of changes

---
+++ 2/main.c
@@ -0,0 +1,12 @@
+// variables and data types
+#include <stdio.h>
+
+// REMEMBER: C is a CASE SENSITIVE language
+
+int main(void)
+{
+    // declaring a variable follows this
+
+    printf("the area of circle =%f\n %d", 3.14 * 3 * 4, 33 * 23);
+    return 0;
+}
